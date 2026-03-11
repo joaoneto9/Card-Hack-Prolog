5ceb040beacb538dc9c070f59e222c2a9dc103ad
Lucas-Cunhaa <lucasgalvao260@gmail.com>
2026-03-11 20:46:01 -0300

    feat: add the game initialization by main and the game title

--- main.pl
+++ main.pl
@@ -1,5 +1,10 @@
 :- use_module('./game_loop').
+:- use_module('./components/cardhack_title', [print_card_hack/0]).
 
 % executar automaticamente assim que o arquivo for carregado.
 % Neste caso, inicia o ciclo principal do jogo de Blackjack.
-:- initialization(game_loop()).
+main :-
+    print_card_hack,
+    game_loop.
+
+:- initialization(main).
